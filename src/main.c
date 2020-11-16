ffea186670b520e4a64700638ab259495c7e5cee
watcol <kantaa058@gmail.com>
2020-11-16 20:47:53 +0900

    Crated simple "cat" program.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,21 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#define MAX_INPUT 50000
 
 int main() {
-  printf("Hello, World!");
+  // Read STDIN.
+  char buf[MAX_INPUT+1] = "";
+  size_t rc = fread(buf, 1, MAX_INPUT, stdin);
+  buf[rc] = '\0';
+
+  // Check whether the file reached EOF.
+  if(getc(stdin) != EOF) {
+    printf("The source is too long.\n");
+    exit(1);
+  }
+
+  // debug: Print the buffer.
+  printf("%s\n", buf);
+  return 0;
 }
