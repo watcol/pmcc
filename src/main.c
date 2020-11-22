c399da1478c2d075711a3d971c6971aa32393539
watcol <kantaa058@gmail.com>
2020-11-22 13:42:47 +0900

    Improved main.c using lex.c

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 
 #define MAX_INPUT 50000
+#define MAX_TOKEN 1000
 
 int main() {
   // Read STDIN.
@@ -18,28 +19,40 @@ int main() {
   // debug: Print a receved number.
   // fprintf(stderr, "%d\n", n);
 
+  int tokens[MAX_TOKEN];
+  char* start[MAX_TOKEN];
+  char* end[MAX_TOKEN];
+
+  buf2tokens(buf, tokens, start, end, MAX_TOKEN);
+
   // Write assembly.
   puts(".intel_syntax noprefix");
   puts(".globl main");
   puts("main:");
 
   // Read a number.
-  printf("  mov rax, %ld\n", strtol(buf, &buf, 10));
+  if (tokens[0] != LEX_NUM) {
+    fprintf(stderr, "The first token should be a number.");
+  }
+  printf("  mov rax, %ld\n", strtol(copy_range(start[0], end[0]), NULL, 10));
 
   // Addition and subtraction.
-  while(*buf) {
-    if (*buf == '+') {
-      buf++;
-      printf("  add rax, %ld\n", strtol(buf, &buf, 10));
-    } else if (*buf == '-') {
-      buf++;
-      printf("  sub rax, %ld\n", strtol(buf, &buf, 10));
-    } else if (*buf == '\n' || *buf == ' ') {
-      // Skip token.
-      buf++;
-    } else {
-      fprintf(stderr, "Unexpected token: '%c'\n", *buf);
-      return 1;
+  for(int i = 1; tokens[i] != LEX_EOF; i++) {
+    if (tokens[i] != LEX_SYMBOL) {
+      fprintf(stderr, "The token after a number should be a symbol.");
+    }
+
+    char* sym = copy_range(start[i], end[i]);
+    i++;
+
+    if (tokens[i] != LEX_NUM) {
+      fprintf(stderr, "The token after a symbol should be a number.");
+    }
+
+    if(*sym == '+') {
+      printf("  add rax, %ld\n", strtol(copy_range(start[i], end[i]), NULL, 10));
+    } else if(*sym == '-') {
+      printf("  sub rax, %ld\n", strtol(copy_range(start[i], end[i]), NULL, 10));
     }
   }
 
