7377806465307a0ecb389ef2e2ce12e6688996a0
watcol <kantaa058@gmail.com>
2020-11-23 16:34:33 +0900

    Updated "expr"

--- src/parse.c
+++ src/parse.c
@@ -3,8 +3,8 @@
 void expr() {
   inst1("push", exp_num());
 
-  while(!eof()) {
-    int o = exp_op();
+  int o;
+  while((o = op())) {
     inst1("push", exp_num());
     inst1("pop", REG_RDI);
     inst1("pop", REG_RAX);
@@ -13,6 +13,9 @@ void expr() {
       inst2("add", REG_RAX, REG_RDI);
     } else if (o == OP_SUB) {
       inst2("sub", REG_RAX, REG_RDI);
+    } else {
+      eputs("Unknown operator");
+      sys_exit(1);
     }
 
     inst1("push", REG_RAX);
