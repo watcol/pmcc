e7f03fec3b36042f1e18ac33c5ad1408c2355318
watcol <kantaa058@gmail.com>
2020-11-23 16:53:42 +0900

    Add Multiplication and Division

--- src/parse.c
+++ src/parse.c
@@ -3,12 +3,37 @@
 int group0[] = {OP_ADD, OP_SUB};
 int count0 = 2;
 
-void expr() {
+int group1[] = {OP_MUL, OP_DIV};
+int count1 = 2;
+
+void expr1() {
   inst1("push", exp_num());
 
   int o;
-  while((o = these_op(group0, count0))) {
+  while((o = these_op(group1, count1))) {
     inst1("push", exp_num());
+    inst1("pop", REG_RDI);
+    inst1("pop", REG_RAX);
+
+    if(o == OP_MUL) {
+      inst2("mul", REG_RAX, REG_RDI);
+    } else if (o == OP_DIV) {
+      inst2("div", REG_RAX, REG_RDI);
+    } else {
+      eputs("Unknown operator");
+      sys_exit(1);
+    }
+
+    inst1("push", REG_RAX);
+  }
+}
+
+void expr() {
+  expr1();
+
+  int o;
+  while((o = these_op(group0, count0))) {
+    expr1();
     inst1("pop", REG_RDI);
     inst1("pop", REG_RAX);
 
@@ -24,6 +49,7 @@ void expr() {
     inst1("push", REG_RAX);
   }
 }
+
 
 void parse(char* buf) {
   init_lexer(buf);
