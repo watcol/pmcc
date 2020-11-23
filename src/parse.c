d8db27f0dfb9ce9a7706e15da8067330f8d957a1
watcol <kantaa058@gmail.com>
2020-11-23 16:13:34 +0900

    Updated with stack machine

--- src/parse.c
+++ src/parse.c
@@ -1,17 +1,25 @@
 #include"teal.h"
 
 void expr() {
-  inst2("mov", REG_RAX, exp_num());
+  inst1("push", exp_num());
 
   while(!eof()) {
     int o = exp_op();
+    inst1("push", exp_num());
+    inst1("pop", REG_RDI);
+    inst1("pop", REG_RAX);
+
     if(o == OP_ADD) {
-      inst2("add", REG_RAX, exp_num());
+      inst2("add", REG_RAX, REG_RDI);
     } else {
-      inst2("sub", REG_RAX, exp_num());
+      inst2("sub", REG_RAX, REG_RDI);
     }
+
+    inst1("push", REG_RAX);
   }
 }
+
+
 
 void parse(char* buf) {
   init_lexer(buf);
@@ -21,5 +29,6 @@ void parse(char* buf) {
 
   expr();
 
+  inst1("pop", REG_RAX);
   inst("ret");
 }
