c3edd409c9cef7189d936bb6399409460f712d1a
Potf Man <potfman@gmail.com>
2020-12-19 19:00:51 +0900

    Add type support for functions.

--- src/parse/func.c
+++ src/parse/func.c
@@ -1,35 +1,39 @@
 #include"parse.h"
 
 int func() {
+  int ty = type();
+  if(!ty) return 0;
   char* name = getCursor();
-  int len = lexIdent();
-  if(!len) return 0;
+  int len = expIdent();
   expThisCh('(');
 
   char* args[MAX_ARGS];
   int arg_lens[MAX_ARGS];
   int arg_tys[MAX_ARGS];
   int c = 0;
-  args[c] = getCursor();
-  arg_lens[c] = lexIdent();
-  arg_tys[c] = TY_I32;
-  if(arg_lens[c]) c++;
-  while(thisCh(',')) {
+
+  arg_tys[c] = type();
+  if(arg_tys[c]) {
     args[c] = getCursor();
     arg_lens[c] = expIdent();
-    arg_tys[c] = TY_I32;
+    c++;
+  }
+
+  while(thisCh(',')) {
+    arg_tys[c] = expType();
+    args[c] = getCursor();
+    arg_lens[c] = expIdent();
     c++;
   }
   expThisCh(')');
 
   if(thisCh(';')) {
-    funcDecl(name, len, TY_I32, arg_tys, c);
+    funcDecl(name, len, ty, arg_tys, c);
     return 1;
   }
 
-  funcBegin(name, len, TY_I32, args, arg_lens, arg_tys, c);
+  funcBegin(name, len, ty, args, arg_lens, arg_tys, c);
   expStmt();
   funcEnd();
   return 1;
 }
-
