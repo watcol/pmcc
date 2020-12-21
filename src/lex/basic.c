c779dc71e69478e383861bbc5bacde4189a34a40
Potf Man <potfman@gmail.com>
2020-12-21 16:52:10 +0900

    Moved lex/ch.c and lex/str.c to lex/basic.c.

--- src/lex/basic.c
+++ src/lex/basic.c
@@ -1,5 +1,22 @@
 #include"lex.h"
 
+char thisCh(char c) {
+  char* cur = getCursor();
+  if(*cur == c) {
+    consume(1);
+    return c;
+  }
+
+  return '\0';
+}
+
+char expThisCh(char c) {
+  if(!thisCh(c)) {
+    panicLex();
+  }
+  return c;
+}
+
 int thisStr(char* str) {
   char* cur = getCursor();
   int len = length(str);
