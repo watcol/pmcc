ec5f12fe5b7027d99ae7b1d9ce62caf46b3e83cc
Potf Man <potfman@gmail.com>
2020-12-22 13:29:46 +0900

    Support pointer calculation.

--- src/gen/gen.c
+++ src/gen/gen.c
@@ -226,44 +226,117 @@ int unaryOp(int op, int var) {
   if(op == OP_DEREF) return derefVar(var);
   if(op == OP_REF) return refVar(var);
 
-  int ty = lVarType(var);
-  int dvar = derefVar(var);
-  int new_var;
-  if(op != OP_NOT) new_var = lTmpVar(llDerefTy(ty));
-
-  if(op == OP_INC) llInstNVAsg("add", new_var, 1, dvar);
-  else if(op == OP_DEC) llInstNVAsg("add", new_var, -1, dvar);
-  else if(op == OP_SUB) llInstNVAsg("sub", new_var, 0, dvar);
-  else if(op == OP_NOT) {
-    int tmp_var = lTmpVar(TY_I1);
-    llIcmpNVAsg("eq", tmp_var, 0, dvar);
-    new_var = lTmpVar(llDerefTy(ty));
-    llConv(new_var, tmp_var);
-  } else {
-    panic("Invalid Unary operator.");
-  }
+  int ty = llDerefTy(lVarType(var));
+  int dvar;
+  int tmp_var;
 
   if(op == OP_INC || op == OP_DEC) {
-    llStore(var, new_var);
+    if(llIsRef(ty)) {
+      dvar = castVar(var, TY_I64);
+      dvar = derefVar(dvar);
+      tmp_var = lTmpVar(TY_I64);
+    } else {
+      dvar = derefVar(var);
+      tmp_var = lTmpVar(ty);
+    }
+    if(op == OP_INC) llInstNVAsg("add", tmp_var, 1, dvar);
+    else llInstNVAsg("add", tmp_var, -1, dvar);
+  } else if(op == OP_SUB) {
+    if(llIsUnsigned(ty)) {
+      panic("Can't negate an unsigned value.");
+      return -2;
+    }
+    dvar = derefVar(var);
+    tmp_var = lTmpVar(ty);
+    llInstNVAsg("sub", tmp_var, 0, dvar);
+  } else if(op == OP_NOT) {
+    dvar = derefVar(var);
+    tmp_var = lTmpVar(TY_I1);
+    llIcmpNVAsg("eq", tmp_var, 0, dvar);
+  } else {
+    panic("Invalid Unary operator.");
+    return -2;
+  }
+
+  int new_var = castVar(refVar(tmp_var), ty);
+
+  if(op == OP_INC || op == OP_DEC) {
+    llStore(var, derefVar(new_var));
     return var;
   }
 
-  return refVar(new_var);
+  return new_var;
 }
 
-int arithOp(int op, int ty, int var1, int var2) {
-  int dvar1 = derefVar(var1);
-  int dvar2 = derefVar(var2);
-  int new_var = lTmpVar(ty);
+int arithOp(int op, int var1, int var2) {
+  int ty1 = llDerefTy(lVarType(var1));
+  int ty2 = llDerefTy(lVarType(var2));
 
-  if(op == OP_ADD) llInstVVAsg("add", new_var, dvar1, dvar2);
-  else if(op == OP_SUB) llInstVVAsg("sub", new_var, dvar1, dvar2);
-  else if(op == OP_MUL) llInstVVAsg("mul", new_var, dvar1, dvar2);
+  int dvar1;
+  int dvar2;
+  int tmp_var;
+  int new_var;
+
+  if(op == OP_ADD) {
+    if(llIsRef(ty1)) {
+      if(llIsRef(ty2)) {
+        panic("Can't add pointer and pointer.");
+        return -2;
+      }
+      var1 = castVar(var1, TY_I64);
+      var2 = castVar(var2, TY_I64);
+      dvar1 = derefVar(var1);
+      dvar2 = derefVar(var2);
+      tmp_var = lTmpVar(TY_I64);
+      llInstVVAsg("add", tmp_var, dvar1, dvar2);
+      return castVar(refVar(tmp_var), ty1);
+    }
+
+    dvar1 = derefVar(var1);
+    var2 = castVar(var2, ty1);
+    dvar2 = derefVar(var2);
+    new_var = lTmpVar(ty1);
+    llInstVVAsg("add", new_var, dvar1, dvar2);
+    return refVar(new_var);
+  } else if(op == OP_SUB) {
+    if(llIsRef(ty1)) {
+      var1 = castVar(var1, TY_I64);
+      var2 = castVar(var2, TY_I64);
+      dvar1 = derefVar(var1);
+      dvar2 = derefVar(var2);
+      tmp_var = lTmpVar(TY_I64);
+      llInstVVAsg("sub", tmp_var, dvar1, dvar2);
+      if(llIsRef(ty2)) {
+        return refVar(tmp_var);
+      } else {
+        return castVar(refVar(tmp_var), ty1);
+      }
+    }
+
+    dvar1 = derefVar(var1);
+    var2 = castVar(var2, ty1);
+    dvar2 = derefVar(var2);
+    new_var = lTmpVar(ty1);
+    llInstVVAsg("sub", new_var, dvar1, dvar2);
+    return refVar(new_var);
+  }
+
+  if(llIsRef(ty1) || llIsRef(ty2)) {
+    panic("Can't multiple or divide pointer.");
+    return -2;
+  }
+
+  dvar1 = derefVar(var1);
+  var2 = castVar(var2, ty1);
+  dvar2 = derefVar(var2);
+  new_var = lTmpVar(ty1);
+
+  if(op == OP_MUL) llInstVVAsg("mul", new_var, dvar1, dvar2);
   else if(op == OP_DIV){
-    if(llIsUnsigned(ty)) llInstVVAsg("udiv", new_var, dvar1, dvar2);
+    if(llIsUnsigned(ty1)) llInstVVAsg("udiv", new_var, dvar1, dvar2);
     else llInstVVAsg("sdiv", new_var, dvar1, dvar2);
   } else if(op == OP_REM) {
-    if(llIsUnsigned(ty)) llInstVVAsg("urem", new_var, dvar1, dvar2);
+    if(llIsUnsigned(ty1)) llInstVVAsg("urem", new_var, dvar1, dvar2);
     else llInstVVAsg("srem", new_var, dvar1, dvar2);
   }
 
@@ -312,8 +385,13 @@ int andOr(int op, int ty, int var1, int var2) {
   return new_var;
 }
 
-int asgOp(int op, int ty, int var1, int var2) {
-  if(op != OP_ASG) var2 = arithOp(op - 17, ty, var1, var2);
+int asgOp(int op, int var1, int var2) {
+  if(op != OP_ASG) var2 = arithOp(op - 17, var1, var2);
+
+  if(lVarType(var2) != lVarType(var1)) {
+    panic("Can't store the different type.");
+    return -2;
+  }
 
   int dvar2 = derefVar(var2);
   llStore(var1, dvar2);
@@ -322,15 +400,28 @@ int asgOp(int op, int ty, int var1, int var2) {
 
 int binOp(int op, int var1, int var2) {
   int ty = llDerefTy(lVarType(var1));
-  var2 = castVar(var2, ty);
+  if(op >= OP_LT && op <= OP_OR) {
+    if(llIsRef(ty)) {
+      ty = TY_I64;
+      var1 = castVar(var1, TY_I64);
+      var2 = castVar(var2, TY_I64);
+    } else {
+      var2 = castVar(var2, ty);
+    }
+  }
 
-  int new_var = -1;
+  int new_var;
 
-  if(op >= OP_ADD && op <= OP_REM) new_var = arithOp(op, ty, var1, var2);
+  if(op >= OP_ADD && op <= OP_REM) new_var = arithOp(op, var1, var2);
   else if(op >= OP_LT && op <= OP_NE) new_var = cmpOp(op, ty, var1, var2);
   else if(op == OP_AND || op == OP_OR) new_var = andOr(op, ty, var1, var2);
-  else if(op >= OP_ASG || op <= OP_REMASG) new_var = asgOp(op, ty, var1, var2);
-  else panic("Invalid binary operator.");
+  else if(op >= OP_ASG || op <= OP_REMASG) new_var = asgOp(op, var1, var2);
+  else {
+    panic("Invalid binary operator.");
+    return -2;
+  }
+
+  if(ty == TY_I64) new_var = castVar(new_var, TY_I32);
 
   return new_var;
 }
